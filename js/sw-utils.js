90c873fdc511453b3b3202e5ca802b0f18b3cf43
JCB <oreyabak@gmail.com>
2019-02-14 20:29:25 +0100

    Commit inicial

---
+++ js/sw-utils.js
@@ -0,0 +1,12 @@
+// Guarda en la cache dinámica.
+function actualizarCacheDinamico(dynamicCache, req, res) { 
+	if (res.ok) {
+		return caches.open(dynamicCache).then(cache => {
+			cache.put(req, res.clone());
+			return res.clone();
+		});
+	}
+	else {
+		return res;
+	}
+}
